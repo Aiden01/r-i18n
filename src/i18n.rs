5e91614dc308c4257fdb6f388e62e8133d1c3d64
Elian Gutierrez <eliangu3600103@gmail.com>
2018-08-31 23:28:43 -0400

    feat(i18n): Read file for translations

--- src/i18n.rs
+++ src/i18n.rs
@@ -1,3 +1,8 @@
+
+use std::fs::File;
+use std::io::Read;
+use std::io::BufReader;
+
 pub struct I18nConfig<'a> {
     pub locales: &'a [&'a str],
     pub directory: &'a str
@@ -12,7 +17,9 @@ pub struct I18n<'b> {
 impl<'b> I18n<'b> {
     pub fn configure(config: &'b I18nConfig<'b>) -> I18n<'b> {
         if let Some(current_lang) = config.locales.get(0) {
-            I18n{config: config, current_lang: current_lang}
+            let i18n: I18n = I18n{config: config, current_lang: current_lang};
+            let buffers = i18n.read_files();
+            i18n
         } else {
             panic!("You must add one language");
         }
@@ -23,6 +30,21 @@ impl<'b> I18n<'b> {
             true => self.current_lang = lang,
             false => panic!("Please add {} to the list.", lang)
         }
+    }
+
+    fn read_files(&self) -> Vec<String> {
+        let mut buffers = Vec::new();
+        &self.config.locales
+                .iter()
+                .map(|file| {
+                    let path = &format!("{}/{}.json", self.config.directory, file);
+                    let file = File::open(path).expect("Failed to open the file");
+                    let mut reader = BufReader::new(file);
+                    let mut contents = String::new();
+                    reader.read_to_string(&mut contents).expect("Failed to read the file");
+                    buffers.push(contents);
+                });
+        buffers
     }
 
 }
